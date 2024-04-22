f0e8bb32558d0bf95adb100abc77ca4481cb3d51
hartheek <hartheekreddy@gmail.com>
2024-04-22 18:11:51 -0500

    first

---
+++ Backend/index.js
@@ -0,0 +1,77 @@
+const express= require('express');
+const cors= require('cors');
+const requestIp= require('request-ip');
+const NodeGeocoder = require('node-geocoder');
+const date = require('date-and-time');
+require("dotenv").config();
+const PORT=8000;
+
+const {Pool} = require('pg');
+const {pool}=require('./database')
+const app= express();
+const axios= require('axios')
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set('trust proxy', true);
+
+app.use((req,res,next)=>{
+    function checkip(ip, ip2) {
+        var ip = ip.split('.').slice(0, 3);
+        var ip2 = ip2.split('.').slice(0, 3);
+    
+        return ip.join('.')=== ip2.join('.');
+    }
+    var ip1= req.ip;
+    var ip='165.134.212';
+    var ip2='165.134.212.81'
+    if(checkip(ip1,ip2)==true){
+        console.log('yes')
+        console.log(ip1)
+        next()
+    }else{
+        res.status(403).send('forbidden')
+    }
+    
+    
+          
+    const getLocation = () => {
+        return new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(resolve, reject);
+        });
+    };
+    
+    // Function to check if the current location is within the expected range
+})
+app.post("/", async(req, res) => {
+    try{
+    const longitude= req.body.longitude
+    const latitude=req.body.latitude
+    if(38.6351275 < latitude && latitude < 38.6353075 && -90.2330823 < longitude && longitude < -90.2310000){
+        const fullname =req.body.name
+        const bannerid=req.body.bannerid
+        const sessionid=req.body.sessionid
+        const now = new Date();
+        const formattedDate = date.format(now, 'YYYY/MM/DD');
+        const formattedTime = date.format(now, 'HH:mm:ss');
+
+        const inserting='INSERT INTO students(sessionid, fullname, bannerid, PresentDate, time) VALUES($1, $2, $3, $4,$5)'
+        await pool.query(inserting,[sessionid, fullname, bannerid, formattedDate, formattedTime])
+        await res.send("data inserted")
+        
+    }
+    else{
+        console.log("hello")
+        res.send("location")
+    }
+    }
+    catch(err){
+        console.error("error",err);
+        res.send("you are caught")
+    }
+    
+});
+
+
+app.listen(process.env.PORT|| PORT);
---
+++ frontend/src/App.js
@@ -0,0 +1,107 @@
+import logo from './logo.svg';
+import './App.css';
+import 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Col, Button, Row, Container, Card, Form } from "react-bootstrap";
+import axios from'axios';
+import React,{useState,state} from 'react'
+// import {internalIpV6, internalIpV4} from 'internal-ip';
+import { ClientJS } from 'clientjs';
+
+
+import date from 'date-and-time';
+function App() {
+  const [data, setData] = useState({
+    name: "",
+    bannerid: "" 
+
+  });
+  const [submitted, setSubmitted] = useState(false);
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // console.log(await internalIpV6());
+    if (!navigator.userAgent.includes("Chrome")) {
+      alert("Please open this website in Google Chrome for the best experience.");
+      window.location.href = "about:blank"; // Redirect to a blank page
+  }
+
+    let latitude = null;
+    let longitude = null;
+
+    try {
+        const position = await new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(resolve, reject);
+        });
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
+        console.log(latitude, longitude)
+    } catch (error) {
+        if (error.code === error.PERMISSION_DENIED) {
+            alert("Please allow location access to use this feature.");
+            return;
+        }
+    }
+  
+      
+    const client = new ClientJS()
+    const sessionid=client.getFingerprint();
+    console.log(client.getFingerprint())
+    const now = new Date();
+    const formattedDate = date.format(now, 'YYYY/MM/DD');
+    const formattedTime = date.format(now, 'HH:mm:ss');
+    
+
+
+    axios.post(`http://localhost:8000`, {...data, sessionid,formattedDate,formattedTime,latitude,longitude}).then((res) => {
+  
+    if(res.data=="location"){
+      alert("you are caught loc")
+    }
+    else if(res.data=="data inserted"){
+      alert("good submitted your attendance")
+
+    }
+    else{
+      alert("you are caught and not allowed")
+    }
+      });
+  };
+
+  return(
+    <Container className="h-100">
+  <Row className="h-100 d-flex justify-content-center align-items-center">
+    <Col md={10} lg={8} xl={6}>
+      <Card className="shadow">
+        <Card.Body>
+          <h2 className="fw-bold mb-4 text-uppercase text-center">Attendance</h2>
+
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <label htmlFor='name' className="form-label">Full Name</label>
+              <input type="text" className="form-control" placeholder="Enter full name" name='name' value={data.name} onChange={handleChange}></input>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="bannerid" className="form-label">Banner ID</label>
+              <input type="text" className="form-control" placeholder="Enter your banner id" name='bannerid' value={data.bannerid} onChange={handleChange}></input>
+            </div>
+            <div className="text-center">
+              <button className='btn btn-primary' type='submit'>Submit</button>
+            </div>
+          </form>
+        </Card.Body>
+      </Card>
+    </Col>
+  </Row>
+</Container>
+
+
+)
+}
+
+export default App;
